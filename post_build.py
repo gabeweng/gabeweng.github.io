039b16b571e3a6892142efcd0292533c541c57a7
Amit Chaudhary <meamitkc@gmail.com>
2020-12-02 12:08:45 +0545

    Remove dependency on fabric

--- post_build.py
+++ post_build.py
@@ -17,3 +17,7 @@ def purge_cloudflare_cache():
 
     response = requests.request('POST', url, data=payload, headers=headers)
     assert response.json()['success'], 'Failed to clear cache'
+
+
+if __name__ == '__main__':
+    purge_cloudflare_cache()
